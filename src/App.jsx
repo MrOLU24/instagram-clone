fcc0442ef26ae9a2c1a4abf5b6c95390b73fe3d5
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-20 23:46:30 +0100

    link the login page to the signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Profile from "./Pages/Profile";
 import Login from "./Pages/Login";
-// import Signup from "./Pages/Signup";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-       <Login/>
+        <Routes>
+          {/* Route for Login */}
+          <Route path="/login" element={<Login />} />
+          {/* Route for Signup */}
+          <Route path="/signup" element={<Signup />} />
+          {/* Default route (redirect to Login) */}
+          <Route path="/" element={<Login />} />
+        </Routes>
       </div>
     </Router>
   );
